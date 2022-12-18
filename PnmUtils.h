d2d2f5d3e5a9637ae935683438da84e7ccbde65c
KenjiCpp <dipper889@gmail.com>
2022-12-18 22:49:36 +0700

    Initial commit

---
+++ PnmUtils.h
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct pixel_t {
+    float r; // Value range [0.0, 1.0]
+    float g; // Value range [0.0, 1.0]
+    float b; // Value range [0.0, 1.0]
+};
+
+pixel_t makePixel(float r, float g, float b) {
+    pixel_t res;
+    res.r = r;
+    res.g = g;
+    res.b = b;
+    return res;
+}
+
+float computeError(const pixel_t* p1, const pixel_t* p2, int n) {
+    float err = 0.0f;
+	for (int i = 0; i < n; i++)
+	{
+		err += abs(p1[i].r - p2[i].r);
+		err += abs(p1[i].g - p2[i].g);
+		err += abs(p1[i].b - p2[i].b);
+	}
+	err *= 255.0f / (n * 3.0f);
+	return err;
+}
+
+int readPnm(const char* fileName, pixel_t*& pixels, int& width, int& height) {
+	FILE * f = fopen(fileName, "r");
+	if (!f) {
+		printf("[ERROR] Unable to read %s\n", fileName);
+		return 1;
+	}
+
+	char type[3];
+	fscanf(f, "%s", type);
+
+	if (!strcmp(type, "P2")) {
+        int maxVal;
+        fscanf(f, "%i", &width);
+	    fscanf(f, "%i", &height);
+	    fscanf(f, "%i", &maxVal);
+        float invMaxVal = 1.0f / maxVal;
+
+        pixels = (pixel_t*)malloc(width * height * sizeof(pixel_t));
+	    for (int i = 0; i < width * height; i++) {
+            int vi;
+		    fscanf(f, "%i", &vi);
+            pixels[i] = makePixel(
+                vi * invMaxVal,
+                vi * invMaxVal,
+                vi * invMaxVal
+            );
+        }
+    }
+	else if (!strcmp(type, "P3")) {
+        int maxVal;
+        fscanf(f, "%i", &width);
+	    fscanf(f, "%i", &height);
+	    fscanf(f, "%i", &maxVal);
+        float invMaxVal = 1.0f / maxVal;
+
+        pixels = (pixel_t*)malloc(width * height * sizeof(pixel_t));
+	    for (int i = 0; i < width * height; i++) {
+            int ri, gi, bi;
+		    fscanf(f, "%i%i%i", &ri, &gi, &bi);
+            pixels[i] = makePixel(
+                ri * invMaxVal,
+                gi * invMaxVal,
+                bi * invMaxVal
+            );
+        }
+	}
+    else {
+        printf("[ERROR] Unsupported PNM type %s\n", type);
+        fclose(f);
+        return 1;
+    }
+
+    fclose(f);
+    return 0;
+}
+
+int writePnm(const char* fileName, pixel_t* pixels, int width, int height) {
+	FILE * f = fopen(fileName, "w");
+	if (!f) {
+		printf("[ERROR] Unable to write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}	
+
+	fprintf(f, "P3\n%i\n%i\n255\n", width, height); 
+
+	for (int i = 0; i < width * height; i++) {
+        int ri = 255 * pixels[i].r;
+        int gi = 255 * pixels[i].g;
+        int bi = 255 * pixels[i].b;
+        fprintf(f, "%i\n%i\n%i\n", ri, gi, bi);
+    }
+		
+	fclose(f);
+    return 0;
+}
---
+++ SeamCarving.cu
@@ -0,0 +1,157 @@
+#include "PnmUtils.h"
+
+#define CHECK(call) {\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess) {\
+        fprintf(stderr, "[CUDA ERROR] %s:%d, code: %d, reason: %s\n", __FILE__, __LINE__, error, cudaGetErrorString(error));\
+        exit(1);\
+    }\
+}
+
+struct GpuTimer {
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer() {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer() {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start() {
+        cudaEventRecord(start, 0);
+    }
+
+    void Stop() {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed() {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
+char* concatStr(const char* s1, const char* s2) {
+    char* result = (char*)malloc(strlen(s1) + strlen(s2) + 1);
+    if (result) {
+        strcpy(result, s1);
+        strcat(result, s2);
+    }
+    return result;
+}
+
+void seamCarving_host(const pixel_t* input, int inputWidth, int inputHeight, pixel_t* output, int outputWidth, int outputHeight) {
+
+}
+
+void seamCarving_device(const pixel_t* input, int inputWidth, int inputHeight, pixel_t* output, int outputWidth, int outputHeight) {
+
+}
+
+int main(int argc, char** argv) {
+    if (argc != 4 && argc != 6) {
+        printf("[ERROR] Invalid number of arguments\n");
+        return 1; 
+    }
+    char* inputFile  = argv[1];
+    char* outputFile = argv[3];
+    int   newWidth   = atoi(argv[2]);
+    int   blockSizeX = 64;
+    int   blockSizeY = 64;
+
+    for (char* c = outputFile; *c != '\0'; ++c) {
+        if (*c == '.') {
+            *c = '\0';
+            break;
+        }
+    }
+
+    if (argc == 6) {
+        blockSizeX = atoi(argv[4]);
+        blockSizeX = atoi(argv[5]);
+    }
+
+    if (blockSizeX <= 0 || blockSizeY <= 0) {
+        printf("[ERROR] Invalid arguments: Block size must be positive\n");
+        return 1; 
+    }
+
+    int      inputWidth  = 0;
+    int      inputHeight = 0;
+    pixel_t* inputPixels = nullptr;
+    if (readPnm(inputFile, inputPixels, inputWidth, inputHeight)) {
+        return 1;
+    }
+
+    if (newWidth <= 0 || newWidth >= inputWidth) {
+        printf("[ERROR] Invalid arguments: New width must be positive and smaller than input image width\n");
+        return 1; 
+    }
+
+    int outputWidth  = newWidth;
+    int outputHeight = inputHeight;
+
+    // Info
+    cudaDeviceProp deviceProps;
+    CHECK(cudaGetDeviceProperties(&deviceProps, 0));
+
+    printf("[INFO] GPU\n");
+    printf("[INFO]     Name                         : %s\n"        , devProv.name);
+    printf("[INFO]     Compute capability           : %d.%d\n"     , devProv.major, devProv.minor);
+    printf("[INFO]     Number of SMs                : %d\n"        , devProv.multiProcessorCount);
+    printf("[INFO]     Max number of threads per SM : %d\n"        , devProv.maxThreadsPerMultiProcessor); 
+    printf("[INFO]     Max number of warps per SM   : %d\n"        , devProv.maxThreadsPerMultiProcessor / devProv.warpSize);
+    printf("[INFO]     GMEM                         : %zu byte\n"  , devProv.totalGlobalMem);
+    printf("[INFO]     SMEM per SM                  : %zu byte\n"  , devProv.sharedMemPerMultiprocessor);
+    printf("[INFO]     SMEM per block               : %zu byte\n\n", devProv.sharedMemPerBlock);
+    printf("[INFO] Input image\n");
+    printf("[INFO]     Width  : %d (pixels)\n"  , inputWidth);
+    printf("[INFO]     Height : %d (pixels)\n\n", inputheight);
+    printf("[INFO] Output image\n");
+    printf("[INFO]     Width  : %d (pixels)\n"  , outputWidth);
+    printf("[INFO]     Height : %d (pixels)\n\n", outputHeight);
+
+    char* hostOutputFile   = concatStr(outputFile, "_host.pnm");
+    char* deviceOutputFile = concatStr(outputFile, "_device.pnm");
+
+    pixel_t* hostOutputPixels   = (pixel_t*)malloc(outputWidth * outputHeight * sizeof(pixel_t));
+    pixel_t* deviceOutputPixels = (pixel_t*)malloc(outputWidth * outputHeight * sizeof(pixel_t));
+    
+    if (!hostOutputPixels || !deviceOutputPixels) {
+        printf("[ERROR] Unable to allocate memory for output\n");
+        return 1;
+    }
+
+    GpuTimer timer;
+
+    // Host
+
+    // Device
+
+
+    if (hostOutputPixels) {
+        free(hostOutputPixels);
+    }
+    if (deviceOutputPixels) {
+        free(deviceOutputPixels);
+    }
+
+    if (inputPixels) {
+        free(inputPixels);
+    }
+    if (hostOutputFile) {
+        free(hostOutputFile);
+    }
+    if (deviceOutputFile) {
+        free(deviceOutputFile);
+    }
+
+    return 0;
+}
